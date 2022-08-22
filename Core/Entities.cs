3b4e39cf9da7708ecc6ec046b7817577112d1279
PhilEZad <90150224+PhilEZad@users.noreply.github.com>
2022-08-22 14:01:39 +0200

    Added validators

--- Core/Entities.cs
+++ Core/Entities.cs
@@ -1,13 +1,19 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Entities;
 
 public class Review
 {
     public int Id { get; set; }
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Headline is required.")]
     public string Headline { get; set; }
+    [Required(ErrorMessage = "Rating is required.")]
     public int Rating { get; set; }
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Name is required.")]
     public string ReviewerName { get; set; }
+    [Required]
     public Movie Movie { get; set; }
     public int MovieId { get; set; }
 }
@@ -15,6 +21,8 @@ public class Review
 public class Movie
 {
     public int Id { get; set; }
+    
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Please Enter a Title.")]
     public string Title { get; set; }
     public string Summary { get; set; }
     public int ReleaseYear { get; set; }
